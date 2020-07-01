1c3b79f21eb8530c767b5566ea52c49c3c5a29e1
AlenaNik <alena.nik.digital@gmail.com>
2020-07-01 20:15:22 +0200

    default global styles and normalise

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import { PrimaryButton } from './components/Buttons'
 import { SecondaryButton } from './components/Buttons'
 import { TernaryButton } from './components/Buttons'
+import { GlobalStyle } from './utils'
 
 const App = () => (
     <div>
         <PrimaryButton>Primary button</PrimaryButton>
         <SecondaryButton>Secondary button</SecondaryButton>
         <TernaryButton>Ternary button</TernaryButton>
+        <GlobalStyle></GlobalStyle>
     </div>
 )
 
