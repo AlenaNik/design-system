b79655e9f69ddb06107a2e29295013ef95f941f0
AlenaNik <alena.nik.digital@gmail.com>
2020-07-05 14:29:22 +0200

    context and preview

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
   stories: ["../src/**/*.stories.(js|mdx)"],
-  addons: ["@storybook/preset-create-react-app",  {
-                                                       name: '@storybook/addon-docs',
-                                                       options: {
-                                                         configureJSX: true,
-                                                         babelOptions: {},
-                                                         sourceLoaderOptions: null,
-                                                       },
-                                                     }
-                                                     ]
+  addons: [
+      "@storybook/preset-create-react-app",
+        {
+          name: '@storybook/addon-docs',
+          options: {
+          configureJSX: true,
+          babelOptions: {},
+          sourceLoaderOptions: null,
+          },
+      },
+      "@storybook/addon-contexts/register",
+      "@storybook/addon-backgrounds/register"
+  ]
 };
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,7 +1,7 @@
 import { blue, neutral, yellow, red, green } from './colors'
 import { primaryFont } from "./typography";
 
-export const props = {
+export const defaultTheme = {
     primaryColor: blue[300],
     primaryColorHover: blue[200],
     primaryColorActive: blue[100],
