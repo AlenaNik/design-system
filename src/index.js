7aca70c7d2b66aed61a64f4b338206c1c9a03a0d
AlenaNik <alena.nik.digital@gmail.com>
2020-07-05 15:37:41 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -1,63 +1,50 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import { ThemeProvider } from 'styled-components'
-import { PrimaryButton } from './components/Buttons'
-import { SecondaryButton } from './components/Buttons'
-import { TertiaryButton } from './components/Buttons'
-import { GlobalStyle, darkTheme, props } from './utils'
-import { SignUpModal } from "./components";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, defaultTheme, darkTheme } from "./utils";
+import { SignUpModal, PrimaryButton } from "./components";
 
 const App = () => {
-        const [useDarkTheme, setUseDarkTheme] = useState(false)
-        const [showModal, setShowModal] = useState(false);
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <PrimaryButton
+        style={{ margin: "0 16px" }}
+        onClick={() => setShowModal(!showModal)}
+      >
+        Show modal
+      </PrimaryButton>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
 
-        return (
-            <ThemeProvider theme={useDarkTheme ? darkTheme : props}>
-                    <button style={{ margin: "0 16px 24px", padding: "8px", background: "none"}}
-                            onClick={() => setUseDarkTheme(true)}
-                    >
-                            Dark Theme
-                    </button>
-                    <button style={{ margin: "0 16px 24px", padding: "8px", background: "none"}}
-                            onClick={() => setUseDarkTheme(false)}
-                    >
-                            Default Theme
-                    </button>
-                <button style={{ margin: "0 16px 24px", padding: "8px", background: "none"}}
-                        onClick={() => setShowModal(!showModal)}
-                >
-                    Toggle Modal
-                </button>
-                    {/*<div style={{ background: useDarkTheme ? props.primaryColor : darkTheme.primaryColor,*/}
-                    {/*    display: "flex",*/}
-                    {/*    width: "100vw",*/}
-                    {/*    height: "100vh",*/}
-                    {/*    alignItems: "center",*/}
-                    {/*    justifyContent: "space-around",*/}
-                    {/*    flexWrap: "wrap"*/}
-                    {/*}}>*/}
-                            {/*<PrimaryButton modifiers="small">Primary button</PrimaryButton>*/}
-                            {/*<SecondaryButton modifiers={["large", "warning", "secondaryButtonWarning"]}>Secondary*/}
-                            {/*        button</SecondaryButton>*/}
-                            {/*<TertiaryButton>Ternary button</TertiaryButton>*/}
-                            {/*<br/>*/}
-                            {/*<PrimaryButton>Primary button</PrimaryButton>*/}
-                            {/*<SecondaryButton>Secondary button</SecondaryButton>*/}
-                            {/*<TertiaryButton>Ternary button</TertiaryButton>*/}
-                            {/*<br/>*/}
-                            {/*<PrimaryButton disabled>Primary button</PrimaryButton>*/}
-                            {/*<SecondaryButton disabled>Secondary button</SecondaryButton>*/}
-                            {/*<TertiaryButton disabled>Ternary button</TertiaryButton>*/}
-                            {/*<br/>*/}
-                            {/*<PrimaryButton modifiers="warning">Primary button</PrimaryButton>*/}
-                            {/*<PrimaryButton modifiers="warning">Primary button</PrimaryButton>*/}
-                            {/*<SecondaryButton modifiers={["large", "warning"]}>Secondary button</SecondaryButton>*/}
-                            {/*<TertiaryButton>Ternary button</TertiaryButton>*/}
-                    {/*</div>*/}
-                        <SignUpModal showModal={showModal} setShowModal={setShowModal}/>
-                    <GlobalStyle />
-            </ThemeProvider>
-        )
-}
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+};
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector("#root"));
