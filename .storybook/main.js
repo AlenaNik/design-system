7dd9d317208575f05b299ab270dcfa476061837a
AlenaNik <alena.nik.digital@gmail.com>
2020-07-05 15:05:26 +0200

    a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,10 @@ module.exports = {
           },
       },
       "@storybook/addon-contexts/register",
-      "@storybook/addon-backgrounds/register"
+      "@storybook/addon-backgrounds/register",
+      "@storybook/addon-actions",
+      "@storybook/addon-knobs",
+      "@storybook/addon-a11y/register",
+
   ]
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import { addParameters } from "@storybook/react";
 import { addDecorator } from "@storybook/react";
 import { withContexts } from "@storybook/addon-contexts/react";
 import { contexts } from "./contexts";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from '@storybook/addon-a11y'
 
 addParameters({
   backgrounds: [
@@ -11,3 +13,5 @@ addParameters({
 });
 
 addDecorator(withContexts(contexts));
+addDecorator(withKnobs);
+addDecorator(withA11y);
